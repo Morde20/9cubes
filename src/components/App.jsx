b4d5a11f0997450decddb99f5cef9f6757b4c231
Morde Yoiris <morde1515@gmail.com>
2021-01-30 17:01:11 +0200

    last one

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "../dist/componentsStyle.css";
 import Body from "./Body";
-import Record from "./Record";
 
 const App = () => {
   return (
     <div>
       <Body />
-      <Record />
     </div>
   );
 };
